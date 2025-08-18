a7a2395420bfd532bf5527e92ed362a0ab56831d
Rohan Ghalme <ghalmerohan265@gmail.com>
2025-08-18 12:08:50 +0530

    impr:example

--- linkedlist.py
+++ linkedlist.py
@@ -7,19 +7,21 @@ next node
 
 '''
 
-class Node:
-    def __init__(self,data):
-        self.data = data
-        self.next = None
-
-a = Node(5)
-b = Node(3)
-c = Node(7)
-
-a.next = b
-b.next = c
-
-head = a
+# class Node:
+#     def __init__(self,data):
+#         self.data = data
+#         self.next = None
+#
+# a = Node(5)
+# b = Node(3)
+# c = Node(7)
+#
+# a.next = b
+# b.prev = a
+# b.next = c
+#
+#
+# head = a
 # print(head.data)
 # print(head.next.data)
 # print(head.next.next.data)
@@ -28,72 +30,74 @@ head = a
 
 # Traverse linked list
 
-def printlinkedlist(head):
-    curr = head
-
-    while curr!=None:
-        print(curr.data,end=' ')
-        curr = curr.next
-
+# def printlinkedlist(head):
+#     curr = head
+#
+#     while curr!=None:
+#         print(curr.data,end=' ')
+#         curr = curr.next
+#
 # printlinkedlist(head)
+# print()
 
 # Insertion at the begining
 
-newNode =Node(4)
-newNode.next = head
-head = newNode
+# newNode =Node(4)
+# newNode.next = head
+# head = newNode
 # printlinkedlist(head)
-
+# print()
 # insertion at the end
 
-newNode = Node(1)
-
-curr = head
-while curr.next!=None:
-    curr=curr.next
-
-curr.next = newNode
+# newNode = Node(1)
+#
+# curr = head
+# while curr.next!=None:
+#     curr=curr.next
+#
+# curr.next = newNode
 # printlinkedlist(head)
+# print()
 
 #insertion at the kth index
 
-k=2
-newNode = Node(6)
-
-curr = head
-for i in range(k-1):
-    curr = curr.next
-
-newNode.next = curr.next
-curr.next = newNode
-
+# k=2
+# newNode = Node(6)
+#
+# curr = head
+# for i in range(k-1):
+#     curr = curr.next
+#
+# newNode.next = curr.next
+# curr.next = newNode
+#
 # printlinkedlist(head)
-
+# print()
 
 # delete the first node
 
-head = head.next
+# head = head.next
 # printlinkedlist(head)
 
 # delete last node
-
-curr = head
-while curr.next.next!=None:
-    curr = curr.next
-
-curr.next = None
+#
+# curr = head
+# while curr.next.next!=None:
+#     curr = curr.next
+#
+# curr.next = None
 # printlinkedlist(head)
-
-# Delete the kth node
-
-k=2
-
-curr = head
-for i in range(k-1):
-    curr = curr.next
-
-curr.next = curr.next.next
-printlinkedlist(head)
+# print()
+# # Delete the kth node
+#
+# k=2
+#
+# curr = head
+# for i in range(k-1):
+#     curr = curr.next
+#
+# curr.next = curr.next.next
+# printlinkedlist(head)
 
 #TYPES OF LINKED LIST:-- i).Singly linked list, ii).Doubly linked list, iii).Circular linked list
 
@@ -110,29 +114,63 @@ printlinkedlist(head)
                    ii).Or you can go back to 
                    iii). 
 '''
-class DoublyNode:
+# class DoublyNode:
+#     def __init__(self,data):
+#         self.data = data
+#         self.next = None
+#         self.prev = None
+#
+# a = Node(5)
+# b = Node(3)
+# c = Node(7)
+#
+# a.next = b
+# b.prev = a
+# b.next = c
+#
+# head = a
+
+# print(head.data)
+# print(head.next.data)
+# print(head.next.next.data)
+
+#CIRCULAR LINKED LIST:--
+'''
+*The last node points back to the first node, instead of None.
+*This creates a loop, so you can keep traversing indefinitely.
+
+#why we use Circular linked list? 
+       
+       i). Endless traversal — you can keep going around without restarting.
+      ii). Efficient for certain problems — like implementing a round-robin scheduler, circular queues, or music playlists that loop.
+      iii). Quick insertion/deletion at both ends without needing to traverse from start each time.
+'''
+
+class Node:
     def __init__(self,data):
         self.data = data
         self.next = None
-        self.prev = None
 
 a = Node(5)
 b = Node(3)
 c = Node(7)
 
 a.next = b
-b.prev = a
 b.next = c
+c.next = a
 
 head = a
-
 print(head.data)
 print(head.next.data)
 print(head.next.next.data)
 
+curr = head
 
-
-
+while True:
+    print(curr.data,end=' ')
+    curr=curr.next
+    if curr==head:
+        break
 
 
 
